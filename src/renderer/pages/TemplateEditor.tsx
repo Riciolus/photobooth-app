d2b1bf27e037ef916c581c3385ce316877106ad1
Riciolus <marcellpramana@gmail.com>
2026-02-10 13:45:44 +0700

    feat: reduce slots & clear background

--- src/renderer/pages/TemplateEditor.tsx
+++ src/renderer/pages/TemplateEditor.tsx
@@ -31,6 +31,20 @@ export default function TemplateEditor({ onSave }: Props) {
     }));
   }
 
+  function handleRemoveBackground() {
+    setTemplate((prev) => ({
+      ...prev,
+      canvas: {
+        width: prev.canvas.width,
+        height: prev.canvas.height,
+        background: {
+          path: null,
+          preview: null,
+        },
+      },
+    }));
+  }
+
   function handleAddSlot() {
     setTemplate((prev) => {
       const firstSlot = prev.slots[0];
@@ -51,6 +65,15 @@ export default function TemplateEditor({ onSave }: Props) {
     });
   }
 
+  function removeLastSlot() {
+    setTemplate((prev) => {
+      return {
+        ...prev,
+        slots: template.slots.slice(0, -1),
+      };
+    });
+  }
+
   function handleSave() {
     const runtime = exportStripTemplate(template);
     onSave(runtime);
@@ -62,19 +85,39 @@ export default function TemplateEditor({ onSave }: Props) {
   return (
     <div className="text-[#232020]  flex h-screen bg-[#ffdfc7]">
       {/* Toolbar */}
-      <div className="max-w-60 space-y-4 px-3 py-8">
+      <div className="max-w-52 w-full space-y-4 px-3 py-8">
         <div className="text-xl text-center font-mono font-semibold italic tracking-tighter">
           Loco Booth
         </div>
 
         <div className="space-y-3 flex flex-col items-center">
-          <Button className="w-40" onClick={handlePickBackground}>
-            Upload Strip
-          </Button>
+          <div className="flex w-40 space-x-1">
+            <Button className="w-full" onClick={handlePickBackground}>
+              Upload Strip
+            </Button>
 
-          <Button className="w-40" onClick={handleAddSlot}>
-            Add Slot
-          </Button>
+            {template.canvas.background.preview && (
+              <Button className="w-18 p-0" onClick={handleRemoveBackground}>
+                <img
+                  className="size-9"
+                  src="./assets/icons/trash-bin.svg"
+                  alt="trash-bin"
+                />
+              </Button>
+            )}
+          </div>
+
+          <div className="flex w-40 space-x-2">
+            <Button className="w-full" onClick={handleAddSlot}>
+              Add Slot
+            </Button>
+
+            {template.slots.length > 0 && (
+              <Button className="w-full" onClick={removeLastSlot}>
+                Remove Slot
+              </Button>
+            )}
+          </div>
 
           <Button className="w-40" disabled={isTemplateInvalid} onClick={handleSave}>
             Next
